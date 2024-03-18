119e1e271cafbae10933adb8380e6309a53d78c0
zainabmirza-eng <zainabmirzafatah99@gmail.com>
2024-03-18 23:18:37 +0300

    added

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+ function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener( 'click', function(){
+        alert('You clicked the button!');
+    });
 }
+
+
